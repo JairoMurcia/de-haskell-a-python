28a4bef091fcb279a7041a33f1f9831bb839b56b
JairoMurcia <49169675+JairoMurcia@users.noreply.github.com>
2019-05-05 18:30:25 -0500

    Add files via upload

--- ejercicios_funcional.py
+++ ejercicios_funcional.py
@@ -1,3 +1,20 @@
+def suma(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+
+def division(a,b):
+    if b==0:
+        return None
+    else:
+        return a/b        
+def producto(a,b):
+    return a*b
+
+
+
 def factorial(x):
     if x==0:
         return 1
@@ -5,14 +22,57 @@ def factorial(x):
         return x*factorial(x-1)
 
 
+def fibonacci(a):
+    if a==0:
+        return 0
+    elif a==1:
+        return 1
+    else:
+        return fibonacci(a-1)+fibonacci(a-2)        
+
+
 def cuadrado(x):
     return x**2
 
+def potencia(a,b):
+    if b==0 or a==1:
+        return 1
+    else:
+        return  a*potencia(a,b-1)
+
+def suma_digitos(a):
+    if a<10:
+        return a
+    else:
+        return suma_digitos(a//10)+a%10    
+
+def diezADos(a):
+    if a <2:
+        return a
+    else:
+        return a%2 +diezADos(a//2)*10    
+def dosADiez(a):
+    if a<2:
+        return  a
+    else:
+        return  a%10 +dosADiez(a//10)*2 
+def siguiente(a):
+    if a>9:
+        return 1
+    else:
+        return a+1
+
+def anterior(a):
+    if a<1:
+        return 9
+    else:
+        return a-1
+
 
 def suma_lista(y):
-    if len[y]==1:
+    if len(y)==1 :
         return y[0]
-    else:
+    elif len(y)!=0:
         return y[0]+suma_lista(y[1:])
 
 def lista_igual(a,b):
@@ -56,5 +116,22 @@ def invertir(x):
 	else:
 		return [x[len(x)-1]]+invertir(x[:len(x)-1])	
 
+def doblar(x):
+    if len(x)==1:
+        return [x[0]*2]
+    else:
+        return [x[0]*2]+cuadrado(x[1:])   
 
-print(invertir([1,2,3,4,5,6]))	
+def ultimo_elemento(x):
+    if len(x)==1 or len(x)==0:
+        return x
+    else:
+        return ultimo_elemento(x[1:])    
+
+def cuadrado(x):
+    if len(x)==1:
+        return [x[0]**2]
+    else:
+        return [x[0]**2]+cuadrado(x[1:])    
+
+
